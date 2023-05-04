6d163da2c777ebcdad0a374b87068640ee25ed25
haiderRizvii <haider.rizvi@devsinc.com>
2023-05-04 18:39:18 +0500

    ATS-Integrations: Dry code and change in styling

--- jobs_integrator.js
+++ jobs_integrator.js
@@ -1,39 +1,37 @@
 class JobRenderService {
-	static #companyDomain = document.currentScript.getAttribute('companyDomain')
+	static #company = document.currentScript.getAttribute('company')
 
     static #renderJobs = (jobs) => {
-        let div = document.createElement('div')
+        const headers = ['Title', 'Locations']
+        const div = document.createElement('div')
         div.classList.add('ats-div')
-        let table = document.createElement('table')
-        table.classList.add('ats-table')
+        const table = document.createElement('table')
+        table.classList.add('jobs-table')
         let tr = document.createElement('tr')
-        tr.classList.add('ats-head')
-        let th = document.createElement('th')
-        th.innerHTML = 'Title'
-        th.classList.add('ats-title')
-        tr.appendChild(th)
-        th = document.createElement('th')
-        th.innerHTML = 'Locations'
-        th.classList.add('ats-locations')
-        tr.appendChild(th)
+        tr.classList.add('jobs-table-header')
+        headers.forEach(header => {
+            let th = document.createElement('th')
+            th.innerHTML = header
+            th.classList.add(`${header.toLowerCase()}`)
+            tr.appendChild(th)
+        })
         table.appendChild(tr)
         jobs.forEach(job => {
             tr = document.createElement('tr')
-            tr.classList.add('ats-data-row')
-            let td = document.createElement('td')
-            td.innerHTML = job.title
-            tr.appendChild(td)
-            td = document.createElement('td')
-            td.innerHTML = job.locations
-            tr.appendChild(td)
-            tr.setAttribute('onClick', `window.open("http://${this.#companyDomain}.localhost:3000/public/jobs/${job.id}")`)
+            tr.classList.add('jobs-data-row')
+            Object.keys(job).slice(1).forEach(key => {
+                const td = document.createElement('td')
+                td.innerHTML = job[key]
+                tr.appendChild(td)
+            })
+            tr.setAttribute('onClick', `window.open("http://${this.#company}.localhost:3000/public/jobs/${job.id}")`)
             table.appendChild(tr)
         })
-        document.getElementById('embed-ats-job').appendChild(table)
+        document.getElementById('embedATSWidget').appendChild(table)
     }
 
     static #fetchAndRenderJobs = () => {
-      return fetch(`http://${this.#companyDomain}.localhost:3000/api/v1/jobs`, {
+      return fetch(`http://${this.#company}.localhost:3000/api/v1/jobs`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
